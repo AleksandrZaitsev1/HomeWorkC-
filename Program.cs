4156e007129f301036c7754a9723d31219f7420b
Aleksandr <zaitsey@yandex.ru>
2023-01-18 12:09:15 +0300

    second

--- Program.cs
+++ Program.cs
@@ -22,3 +22,32 @@ else
     max = b;
 }
 Console.WriteLine("max = " + max + "и" + " min = " + min  );*/
+
+/*Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.*/
+
+
+int  a , b , c;
+
+Console.Write(" Введите Имя   ");
+string? username = Console.ReadLine();
+Console.Write("Привет!");
+Console.WriteLine(username);
+
+Console.WriteLine("Введите число a");
+ a = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите число b");
+ b = Convert.ToInt32(Console.ReadLine());
+ Console.WriteLine("Введите число c");
+ c = Convert.ToInt32(Console.ReadLine());
+
+ int max = a;
+
+ if (b > max)
+ {
+    max = b;
+ }  
+ if ( c > max)
+ {
+    max = c;
+ }
+ Console.WriteLine("max = " + max);
