398e3890d4102ae1ce2c9ec79457e20f5be48067
Aleksandr <zaitsey@yandex.ru>
2023-01-18 11:50:42 +0300

    ferst

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿/*int max = 0;
+int min = 0;
+int  a, b;
+
+Console.Write(" Введите Имя   ");
+string? username = Console.ReadLine();
+Console.Write("Привет!");
+Console.WriteLine(username);
+
+Console.WriteLine("Введите число a");
+ a = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите число b");
+ b = Convert.ToInt32(Console.ReadLine());
+if (a>b)
+{
+   max = a;
+   min = b;
+}
+else 
+{
+    min = a;
+    max = b;
+}
+Console.WriteLine("max = " + max + "и" + " min = " + min  );*/
